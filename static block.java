4d16d5bb155b9f489133f003d077e1c9552adce5
Ahileshanbu <ahileshanbalagan@gmail.com>
2019-10-22 19:32:54 +0530

    Create static block.java

---
+++ static block.java
@@ -0,0 +1,29 @@
+package practice;
+
+
+class scale
+{
+	static
+	{
+		num1 = 0;
+		num2 = 0;
+	}
+	public static int num1;
+	public static int num2;
+	
+	public static void disp()
+	{
+		System.out.println(num1+num2);
+	}
+}
+
+public class Static {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		scale.disp();
+
+	}
+
+}
